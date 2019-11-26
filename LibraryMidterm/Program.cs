c30dd7c1464bb8538e33d6dec92591efd540737d
Kate Hyland <katehyland@quickenloans.com>
2019-11-25 19:24:26 -0500

    First commit

---
+++ LibraryMidterm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryMidterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
